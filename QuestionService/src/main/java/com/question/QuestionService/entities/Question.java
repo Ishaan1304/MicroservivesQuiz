02bb69a427c2381f64e67e8856d51433af6ff65a
Ishaan1304 <ishaangangrade1@gmail.com>
2025-11-16 15:56:25 +0530

    First commit

---
+++ QuestionService/src/main/java/com/question/QuestionService/entities/Question.java
@@ -0,0 +1,34 @@
+package com.question.QuestionService.entities;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    private String question;
+
+    private Long quizId;
+
+    @ElementCollection
+    private List<String> options;
+
+    private String correctAnswer;
+}
